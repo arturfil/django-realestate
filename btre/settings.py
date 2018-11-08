3efbd5dd7055f5d37732d02fcb7c3e5909eae557
Arturo Filio Villa <arturfil@hotmail.com>
2018-11-08 12:28:41 -0500

    done the form submission with user signed in, getting error with visitor

--- btre/settings.py
+++ btre/settings.py
@@ -34,8 +34,8 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
-    'contacts.apps.ContactsConfig', 
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -142,3 +142,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email config 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='filioarturo@gmail.com'
+EMAIL_HOST_PASSWORD='Albertbrocoli007'
+EMAIL_USE_TLS=True
