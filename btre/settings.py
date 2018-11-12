27f1cc862636f44251ad25de17e43a6c79cbe8c2
Arturo Filio Villa <arturfil@hotmail.com>
2018-11-11 22:19:18 -0500

    added code for production

--- btre/settings.py
+++ btre/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='filioarturo@gmail.com'
 EMAIL_HOST_PASSWORD='Albertbrocoli007'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
